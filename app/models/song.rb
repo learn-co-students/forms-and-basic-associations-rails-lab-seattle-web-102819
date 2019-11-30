e13ed2837c334446b844e2725995054622e4f008
bahayg <bahayg@gmail.com>
2019-11-30 13:32:23 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belong_to :genre
+
+  def artist_name=(name)
+    @artist = name
+  end
+
+  def artist_name
+    artist.name
+  end
+
 end
